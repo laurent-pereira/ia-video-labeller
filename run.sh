a47eee6c1324d709b5053da4d2a986d95f433e37
laurent-pereira <laurent.pereira1@gmail.com>
2025-06-09 18:36:27 +0200

    remove deprecated file

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🖥️ Autorisation X11 (nécessaire pour l'affichage temps réel)..."
-xhost +local:docker
-echo "🚀 Lancement du traitement YOLO..."
-docker-compose up
-echo "🧹 Nettoyage..."
-xhost -local:docker
